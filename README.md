fb0abe5af0822c86ce08b78f62d1deb84e3f204c
Eugen Hruska <eh22@rice.edu>
2020-03-12 17:37:21 -0500

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# run commands 
+```
+./compile.sh
---
+++ compile.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+clear
+
+file='thesis_main'
+bibfile='thesis_main'
+
+pdflatex $file'.tex'
+bibtex $bibfile 
+pdflatex $file'.tex'
+pdflatex $file'.tex'
+
+#'.bib'
+
+
+#rm $bibfile'.bbl' $file'.blg'
+# $file'.idx'  $file'.lof' $file'.lot' $file'.toc'
+
+echo 'ok'
+rm .pdf  q.pdf
+rm *aux *lot *idx *lof *bbl *blg *toc
+rm $file'.log' #$file'.aux' 
+echo 'ok'
+ "/c/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/AcroRd32.exe" $file.pdf
